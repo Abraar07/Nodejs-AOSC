474dd316218bc961a8e58aaf666fc34c632ed54e
Abraar07 <mohammedabraar05@gmail.com>
2022-10-22 22:19:05 +0530

    till loading error

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import { legacy_createStore, applyMiddleware, compose } from "redux";
+import thunk from 'redux-thunk';
+import reducers from "./reducers"
+
+const store = legacy_createStore(reducers, compose(applyMiddleware(thunk)))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- server/index.js
+++ server/index.js
@@ -6,11 +6,11 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts",postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use("/posts",postRoutes);
+
 
 // mongodb connection
 const CONNECTION_URL = "mongodb+srv://memory:memory1213@abscluster.f5vt7.mongodb.net/?retryWrites=true&w=majority";
